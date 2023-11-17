c1ac3822575c849ba8b21f4f8c4f1aa289345ad4
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-11-17 11:37:24 +0200

    feat: add some solutions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -236,8 +236,9 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const pad = (value) => `${value}`.padStart(2, '0');
+  return `${pad(minutes)}:${pad(seconds)}`;
 }
 
 /**
@@ -250,8 +251,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -265,8 +266,8 @@ function reverseString(/* str */) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
@@ -281,8 +282,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -316,8 +317,16 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const reversedStr = str
+    .replace(' ', '')
+    .split('')
+    .reverse()
+    .join('')
+    .toLowerCase();
+  const normalizedStr = str.replace(' ', '').toLowerCase();
+  return reversedStr === normalizedStr;
+  // TODO: update to check for symbols
 }
 
 /**
